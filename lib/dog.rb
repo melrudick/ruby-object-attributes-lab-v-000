209412d6776fae93b5b79608efd9892f0d2587e7
Melissa Rudick <mel.madrugada@gmail.com>
2017-09-03 06:32:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+
+    def name
+      @name
+    end
+end
+    def breed=(dog_breed)
+      @breed = dog_breed
+    end
+
+    def breed
+      @breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+
+
+      def name=(person_name)
+        @name = person_name
+
+      def name
+        @name
+
+      # beyonce = Person.new
+      # beyonce.name = "Beyonce"
+    end
+  end
+
+
+
+  def job=(job_name)
+    @job = job_name
+
+  def job
+    @job
+
+  end
+end
+end
